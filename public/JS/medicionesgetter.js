8f362f8b32d3bf7dbd4c81d6f2479defb992efc1
smariba <smaribar@epsg.upv.es>
2024-10-07 13:23:21 +0200

    Comentado para doxygen

--- public/JS/medicionesgetter.js
+++ public/JS/medicionesgetter.js
@@ -1,5 +1,16 @@
-//Recoger la ultima medicion de la base de datos
+/**
+ * @file medicionesgetter.js
+ * @brief Funciones para obtener mediciones de la base de datos y mostrarlas en la página web.
+ * @details Este archivo contiene funciones para obtener mediciones de la base de datos y mostrarlas en la página web.
+ * **/
 
+/**
+ * @function getUltimaMedicion
+ * @brief Obtiene la última medición registrada en la base de datos.
+ * @details Realiza una petición GET a la ruta "/ultima-medicion" para obtener la última medición registrada en la base de datos.
+ * @returns La última medición registrada en la base de datos.
+ * @throws Error si hay un error al obtener la última medición.
+ * **/
 const getUltimaMedicion = async () => {
     try {
         const response = await fetch('/ultima-medicion');
@@ -12,11 +23,22 @@ const getUltimaMedicion = async () => {
         return null;
     }
 };
-
+/**
+ * @interface Botón para obtener la última medición
+ * @brief Interfaz para obtener la última medición registrada en la base de datos.
+ * **/
 //darle al boton y cambiar el texto h1 por la ultima medicion
 const boton = document.getElementById('btn-ultima-medicion');
+/**
+ * @interface Texto para mostrar la última medición
+ * @brief Interfaz para mostrar la última medición registrada en la base de datos.
+ * **/
 const texto = document.getElementById('texto-medicion');
-
+/**
+ * @function Evento click del botón para obtener la última medición
+ * @brief Evento click del botón para obtener la última medición registrada en la base de datos.
+ * @details Al hacer click en el botón, se obtiene la última medición registrada en la base de datos y se muestra en la página web.
+ * **/
 boton.addEventListener('click', async () => {
     const medicion = await getUltimaMedicion();
     if (medicion) {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+/**
+ * @file index.js
+ * @brief Servidor web para el proyecto de Biometría y Medio Ambiente
+ **/
 import express from 'express';
 import { createPool } from 'mysql2/promise';
 import cors from 'cors'; // Importar CORS
 
+/**
+ * @brief Crear la aplicación de Express
+ * @details Se crea la aplicación de Express y se configura para usar JSON y CORS
+ **/
 const app = express();
 // Use the express.json() middleware to parse JSON bodies
 app.use(express.json());
 // Usar middleware CORS y Morgan
 app.use(cors());
 
+/**
+ * @brief Crear el pool de conexiones con MySQL
+ * @details Se crea el pool de conexiones con MySQL y se configura para conectarse a la base de datos "my
+ * db" en el host "mysqldb" con el usuario "root" y la contraseña "123456".
+ * @param host - El host de la base de datos.
+ * @param user - El usuario de la base de datos.
+ * @param password - La contraseña de la base de datos.
+ * @param database - El nombre de la base de datos.
+ * @param port - El puerto de la base de datos.
+ * **/
 // Crear el pool de conexiones con MySQL
 const pool = createPool({
     host: 'mysqldb',  // Cambiar a 'localhost' o '127.0.0.1' si es necesario
@@ -20,12 +38,30 @@ const pool = createPool({
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
 
-// Redirigir la ruta principal a "/index.html"
+/**
+ * @brief Se redirige la ruta principal a "/index.html"
+ * @details Se redirige la ruta principal a "/index.html" para que al ingresar a la dirección raíz de la
+ * aplicación se cargue el archivo "index.html" de la carpeta "public".
+ **/
 app.get('/', (req, res) => {
     res.redirect('/index.html');
 });
 
-// Ruta para verificar la conexión a la base de datos (ping)
+/**
+ * @function GET /mediciones
+ * @brief Ruta para verificar la conexión a la base de datos (ping)
+ * @details Se crea una ruta "/ping" que devuelve la fecha y hora actuales de la base de datos para verificar
+ * que la conexión está funcionando correctamente.
+ * @returns La fecha y hora actuales de la base de datos.
+ * @throws Error 500 si hay un error al hacer ping a la base de datos.
+ *
+ * @example GET /ping
+ * {
+ *  "0": {
+ *    "NOW()": "2021-10-20T00:00:00.000Z"
+ *    }
+ * }
+ * **/
 app.get('/ping', async (req, res) => {
     try {
         const [result] = await pool.query('SELECT NOW()');
@@ -36,6 +72,21 @@ app.get('/ping', async (req, res) => {
     }
 });
 
+/**
+ * @brief Ruta para obtener la última medición
+ * @details Se crea una ruta "/ultima-medicion" que devuelve la última medición registrada en la base de datos.
+ * @returns La última medición registrada en la base de datos.
+ * @throws Error 404 si no se encontraron mediciones.
+ * @throws Error 500 si hay un error en la base de datos.
+ *
+ * @example GET /ultima-medicion
+ * {
+ *   "fecha": "2021-10-20T00:00:00.000Z",
+ *   "Lugar": "Laboratorio",
+ *   "Gas": "CO2",
+ *   "Valor": 400
+ *   }
+ **/
 // Ruta para obtener la última medición
 app.get('/ultima-medicion', async (req, res) => {
     try {
@@ -49,7 +100,24 @@ app.get('/ultima-medicion', async (req, res) => {
         res.status(500).send('Error en la base de datos');
     }
 });
-
+/**
+ * @brief Ruta para insertar una medición
+ * @details Se crea una ruta "/insertar" que permite insertar una nueva medición en la base de datos.
+ * @param Lugar - El lugar de la medición.
+ * @param Gas - El tipo de gas medido.
+ * @param Valor - El valor de la medición.
+ * @returns Un mensaje de éxito o error en la inserción.
+ * @throws Error 400 si faltan datos obligatorios.
+ * @throws Error 500 si hay un error en el servidor.
+ *
+ * @example POST /insertar
+ * {
+ *    "Fecha": "2021-10-20T00:00:00.000Z",
+ *    "Lugar": "Laboratorio",
+ *    "Gas": "CO2",
+ *    "Valor": 400
+ * }
+ * **/
 app.post('/insertar', async (req, res) => {
     try {
         const { Lugar, Gas, Valor } = req.body; // Obtenemos Lugar, Gas, y Valor del body
